c2d140a7904efea19221c35884b6d864b3fd6eb3
Logan Garber <logangarber@hotmail.com>
2019-06-02 17:20:07 -0400

    VS autoformatting makes me sad

--- DownloadFromBrowser/Program.cs
+++ DownloadFromBrowser/Program.cs
@@ -1,25 +1,20 @@
 ﻿using System.IO;
 
-namespace DownloadFromBrowser
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace DownloadFromBrowser {
+    class Program {
+        static void Main(string[] args) {
             //location to save files to
             string filepath = "C:\\Users\\Logan\\Downloads" + "\\";
 
             //just checks to prevent crashes
-            if (args.Length > 0)
-            {
+            if (args.Length > 0) {
                 //gets the file name
                 int pos = args[0].LastIndexOf('\\');
                 string filename = args[0].Substring(pos + 1);
                 filepath = (filepath + filename);
 
                 //makes sure it has a unique name
-                while (File.Exists(filepath))
-                {
+                while (File.Exists(filepath)) {
                     filepath = (filepath + 1);
                 }
 
