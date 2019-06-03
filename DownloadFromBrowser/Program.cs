381c23f1776d47540f74114da519779be1192291
Logan Garber <logangarber@hotmail.com>
2019-06-02 23:17:22 -0400

    fixed a bug with files with file extensions changed functionality for unique name creation to match usual browser functionality

--- DownloadFromBrowser/Program.cs
+++ DownloadFromBrowser/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DownloadFromBrowser {
     class Program {
@@ -11,15 +13,35 @@ namespace DownloadFromBrowser {
                 //gets the file name
                 int pos = args[0].LastIndexOf('\\');
                 string filename = args[0].Substring(pos + 1);
-                filepath = (filepath + filename);
+
+                //gets the file extension (if exists)
+                string fileExt = "";
+                pos = filename.IndexOf('.');
+                if (pos != -1) {
+                    fileExt = filename.Substring(pos);
+                    filename = filename.Substring(0, pos);
+                }
+
+
+                //puts file together without extension
+                filepath += filename;
 
                 //makes sure it has a unique name
-                while (File.Exists(filepath)) {
-                    filepath = (filepath + 1);
+                while (File.Exists(filepath + fileExt)) {
+                    Regex rx = new Regex(@"\(\d+\)$");
+                    Match m = rx.Match(filepath);
+                    if (m.Success) {
+                        int num = Convert.ToInt32(m.Value.Substring(1, m.Value.Length - 2));
+                        num += 1;
+                        filepath = filepath.Substring(0, m.Index) + "(" + num + ")";
+
+                    } else {
+                        filepath += "(1)";
+                    }
                 }
 
                 //saves the file from temp folder
-                File.Move(args[0], filepath);
+                File.Move(args[0], filepath + fileExt);
             }
         }
     }
